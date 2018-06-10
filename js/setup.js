51fdaff80f4033c143593a5b10dbd4067a411dec
AlinaDenisova <denicov_vasya@mail.ru>
2018-06-10 12:07:55 +0400

    Добавляет генерацию волшебников

---
+++ js/setup.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var wizards = [];
+
+var getRandomData = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var randomWizards = function () {
+  for (var i = 0; i < 4; i++) {
+    var randomName = getRandomData(WIZARD_NAMES) + getRandomData(WIZARD_LAST_NAMES);
+    var wizard = {
+      name: randomName,
+      coatColor: getRandomData(WIZARD_COAT_COLOR),
+      eyesColor: getRandomData(WIZARD_EYES_COLOR)
+    };
+  }
+};
+
+randomWizards();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
